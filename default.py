77234f5884b50e5d2c364f0521cbd3b66b635b3e
Patrick Carey <paddy@wackwack.co.uk>
2012-02-23 03:04:01 +0000

    Split feeds into seperate file to keep default.py as simple as possible
    Added some new feeds

--- default.py
+++ default.py
@@ -7,6 +7,8 @@ import xbmcaddon
 import xbmcgui
 import xbmcplugin
 
+from resources.lib import feeds
+
 ### get addon info
 __addon__             = xbmcaddon.Addon()
 __addonidint__        = int(sys.argv[1])
@@ -14,13 +16,13 @@ __addonid__         = __addon__.getAddonInfo('id')
 __addondir__          = xbmc.translatePath(__addon__.getAddonInfo('path'))
 
 
-def addFeed(title, targetPath, thumbPath = 'DefaultFolder.png', fanartPath = os.path.join(__addondir__, 'fanart.jpg')):
+def addFeed(title, url, thumbnail = 'DefaultFolder.png', fanart = os.path.join(__addondir__, 'fanart.jpg')):
     
     """Add a link to the specified feed"""
     
-    li = xbmcgui.ListItem( label = title, iconImage = thumbPath, thumbnailImage = thumbPath)
-    li.setProperty( "Fanart_Image", fanartPath )
-    xbmcplugin.addDirectoryItem( handle = __addonidint__, url = targetPath, listitem = li, isFolder = True)
+    li = xbmcgui.ListItem( label = title, iconImage = thumbnail, thumbnailImage = thumbnail)
+    li.setProperty( "Fanart_Image", fanart )
+    xbmcplugin.addDirectoryItem( handle = __addonidint__, url = url, listitem = li, isFolder = True)
 
 
 def log(txt='', severity=xbmc.LOGDEBUG):
@@ -33,91 +35,9 @@ def log(txt='', severity=xbmc.LOGDEBUG):
 
 if __name__ == "__main__":
 
-    # Bellator MMA (Youtube)
-    addFeed(    title = 'Bellator MMA (Youtube)', 
-                targetPath = 'plugin://plugin.video.youtube/?path=/root/contacts&folder=true&contact=BellatorMMA&store=contact_options&',
-                thumbPath = 'http://mmaartwork.wackwack.co.uk/fightcasts/bellator.jpg')
-
-    # Eddie Bravo (Youtube)
-    addFeed(    title = 'Eddie Bravo (Youtube)', 
-                targetPath = 'plugin://plugin.video.youtube/?path=/root/contacts&folder=true&contact=twistereddie&store=contact_options&',
-                thumbPath = 'http://mmaartwork.wackwack.co.uk/fightcasts/twistereddie.jpg')
-
-    # Evolve Mixed Martial Arts (Youtube)
-    addFeed(    title = 'Evolve Mixed Martial Arts (Youtube)', 
-                targetPath = 'plugin://plugin.video.youtube/?path=/root/contacts&folder=true&contact=evolvemma&store=contact_options&',
-                thumbPath = 'http://mmaartwork.wackwack.co.uk/fightcasts/evolvemma.jpg')
-
-    # HDNet Fights (Youtube)
-    addFeed(    title = 'HDNet Fights (Youtube)',
-                targetPath = 'plugin://plugin.video.youtube/?path=/root/contacts&folder=true&contact=HDNetFights&store=contact_options&',
-                thumbPath = 'http://mmaartwork.wackwack.co.uk/fightcasts/hdnetfights.jpg')
-
-    # Iron Forges Iron (Youtube)
-    addFeed(    title = 'Iron Forges Iron (Youtube)',
-                targetPath = 'plugin://plugin.video.youtube/?path=/root/contacts&folder=true&contact=ironforgesiron&store=contact_options&',
-                thumbPath = 'http://mmaartwork.wackwack.co.uk/fightcasts/ironforgesiron.jpg')
-
-    # Middleeasy TV (Youtube)
-    addFeed(    title = 'Middleeasy TV (Youtube)', 
-                targetPath = 'plugin://plugin.video.youtube/?path=/root/contacts&folder=true&contact=MiddleEasyTV&store=contact_options&',
-                thumbPath = 'http://mmaartwork.wackwack.co.uk/fightcasts/middleeasy.jpg')
-
-    # MMAFighting.com (Plugin)
-    addFeed(    title = 'MMAFighting.com (Plugin)',
-                targetPath = 'plugin://plugin.video.mmafighting',
-                thumbPath = 'http://mmaartwork.wackwack.co.uk/fightcasts/mmafighting.png')
-
-    # MMAinterviews.tv (Youtube)
-    addFeed(    title = 'MMAinterviews.tv (Youtube)',
-                targetPath = 'plugin://plugin.video.youtube/?path=/root/contacts&folder=true&contact=MMAinterviews&store=contact_options&',
-                thumbPath = 'http://mmaartwork.wackwack.co.uk/fightcasts/mmainterviews.jpg')
-
-    # MMAWeekly.com Videos (Youtube)
-    addFeed(    title = 'MMAWeekly.com Videos (Youtube)',
-                targetPath = 'plugin://plugin.video.youtube/?path=/root/contacts&folder=true&contact=MMAWeeklyVideos&store=contact_options&',
-                thumbPath = 'http://mmaartwork.wackwack.co.uk/fightcasts/mmaweekly.jpg')
-
-    # MMA H.E.A.T. (Youtube)
-    addFeed(    title = 'MMA H.E.A.T. (Youtube)',
-                targetPath = 'plugin://plugin.video.youtube/?path=/root/contacts&folder=true&contact=mmaheat&store=contact_options&',
-                thumbPath = 'http://mmaartwork.wackwack.co.uk/fightcasts/mmaheat.jpg')
-
-    # MMA: Inside the Cage (Youtube)
-    addFeed(    title = 'MMA: Inside the Cage (Youtube)',
-                targetPath = 'plugin://plugin.video.youtube/?path=/root/contacts&folder=true&contact=MMAInsideTheCageTV&store=contact_options&',
-                thumbPath = 'http://mmaartwork.wackwack.co.uk/fightcasts/MMAInsideTheCageTV.jpg')
-
-    # MMA Nuts (RSS)
-    addFeed(    title = 'MMA Nuts (RSS)',
-                targetPath = 'rss://mmanuts.com:80/feed/podcast',
-                thumbPath = 'http://mmaartwork.wackwack.co.uk/fightcasts/mmanuts.jpg')
-
-    # Disabled until Vimeo login is working again
-    ## The Reem (Vimeo)
-    #addFeed(    title = 'The Reem (Vimeo)',
-    #            targetPath = 'plugin://plugin.video.vimeo/?path=/root/contacts&folder=true&contact=thereem&store=contact_options&',
-    #            thumbPath = 'http://mmaartwork.wackwack.co.uk/fightcasts/thereem.jpg')    
-
-    # StillW1ll (Youtube)
-    addFeed(    title = 'StillW1ll (Youtube)',
-                targetPath = 'plugin://plugin.video.youtube/?path=/root/contacts&folder=true&contact=StillW1ll&store=contact_options&',
-                thumbPath = 'http://mmaartwork.wackwack.co.uk/fightcasts/stillwill.jpg')
-
-    # UFC (Youtube)
-    addFeed(    title = 'UFC (Youtube)',
-                targetPath = 'plugin://plugin.video.youtube/?path=/root/contacts&folder=true&contact=UFC&store=contact_options&',
-                thumbPath = 'http://mmaartwork.wackwack.co.uk/fightcasts/ufc.jpg')
-
-    # Whoa! TV (Youtube)
-    addFeed(    title = 'WHOA! TV (Youtube)',
-                targetPath = 'plugin://plugin.video.youtube/?path=/root/contacts&folder=true&contact=whoatv&store=contact_options&',
-                thumbPath = 'http://mmaartwork.wackwack.co.uk/fightcasts/whoatv.jpg')
-
-    # www.MMA30.com (Youtube)
-    addFeed(    title = 'www.MMA30.com (Youtube)',
-                targetPath = 'plugin://plugin.video.youtube/?path=/root/contacts&folder=true&contact=MMA30tv&store=contact_options&',
-                thumbPath = 'http://mmaartwork.wackwack.co.uk/fightcasts/MMA30tv.jpg')
+    # add feeds to directory listing
+    for feed in feeds.video:
+        addFeed( title = feed['title'], url = feed['url'], thumbnail = feed['thumbnail'], fanart = feed['fanart'] )
 
     ## finish adding items to list and display
     xbmcplugin.endOfDirectory(__addonidint__)
---
+++ resources/lib/feeds.py
@@ -0,0 +1,113 @@
+"""
+List of feeds to be used in default.py
+
+Skeleton feed:
+
+    {   'title'     : '',
+        'url'       : '',
+        'thumbnail' : '',
+        'fanart'    : '', } , """
+
+video = [ 
+
+    {   'title'     : 'Bellator MMA (Youtube)',
+        'url'       : 'plugin://plugin.video.youtube/?path=/root/contacts&folder=true&contact=BellatorMMA&store=contact_options&',
+        'thumbnail' : 'http://mmaartwork.wackwack.co.uk/fightcasts/bellator.jpg',
+        'fanart'    : 'http://mmaartwork.wackwack.co.uk/promotions/Bellator-fanart.jpg', } ,
+    
+    {   'title'     : 'Eddie Bravo (Youtube)',
+        'url'       : 'plugin://plugin.video.youtube/?path=/root/contacts&folder=true&contact=twistereddie&store=contact_options&',
+        'thumbnail' : 'http://mmaartwork.wackwack.co.uk/fightcasts/twistereddie.jpg',
+        'fanart'    : '', } ,
+    
+    {   'title'     : 'Evolve Mixed Martial Arts (Youtube)',
+        'url'       : 'plugin://plugin.video.youtube/?path=/root/contacts&folder=true&contact=evolvemma&store=contact_options&',
+        'thumbnail' : 'http://mmaartwork.wackwack.co.uk/fightcasts/evolvemma.jpg',
+        'fanart'    : '', } ,
+
+    {   'title'     : 'HDNet Fights (Youtube)',
+        'url'       : 'plugin://plugin.video.youtube/?path=/root/contacts&folder=true&contact=HDNetFights&store=contact_options&',
+        'thumbnail' : 'http://mmaartwork.wackwack.co.uk/fightcasts/hdnetfights.jpg',
+        'fanart'    : '', } ,
+
+    {   'title'     : 'Iron Forges Iron (Youtube)',
+        'url'       : 'plugin://plugin.video.youtube/?path=/root/contacts&folder=true&contact=ironforgesiron&store=contact_options&',
+        'thumbnail' : 'http://mmaartwork.wackwack.co.uk/fightcasts/ironforgesiron.jpg',
+        'fanart'    : '', } ,
+
+    {   'title'     : 'Middleeasy TV (Youtube)',
+        'url'       : 'plugin://plugin.video.youtube/?path=/root/contacts&folder=true&contact=MiddleEasyTV&store=contact_options&',
+        'thumbnail' : 'http://mmaartwork.wackwack.co.uk/fightcasts/middleeasy.jpg',
+        'fanart'    : '', } ,
+
+    {   'title'     : 'MMAFighting.com (Plugin)',
+        'url'       : 'plugin://plugin.video.mmafighting',
+        'thumbnail' : 'http://mmaartwork.wackwack.co.uk/fightcasts/mmafighting.png',
+        'fanart'    : '', } ,
+
+    {   'title'     : 'MMAinterviews.tv (Youtube)',
+        'url'       : 'plugin://plugin.video.youtube/?path=/root/contacts&folder=true&contact=MMAinterviews&store=contact_options&',
+        'thumbnail' : 'http://mmaartwork.wackwack.co.uk/fightcasts/mmainterviews.jpg',
+        'fanart'    : '', } ,
+
+    {   'title'     : 'MMAWeekly.com Videos (Youtube)',
+        'url'       : 'plugin://plugin.video.youtube/?path=/root/contacts&folder=true&contact=MMAWeeklyVideos&store=contact_options&',
+        'thumbnail' : 'http://mmaartwork.wackwack.co.uk/fightcasts/mmaweekly.jpg',
+        'fanart'    : '', } ,
+
+    {   'title'     : 'MMA H.E.A.T. (Youtube)',
+        'url'       : 'plugin://plugin.video.youtube/?path=/root/contacts&folder=true&contact=mmaheat&store=contact_options&',
+        'thumbnail' : 'http://mmaartwork.wackwack.co.uk/fightcasts/mmaheat.jpg',
+        'fanart'    : '', } ,
+
+    {   'title'     : 'MMA: Inside the Cage (Youtube)',
+        'url'       : 'plugin://plugin.video.youtube/?path=/root/contacts&folder=true&contact=MMAInsideTheCageTV&store=contact_options&',
+        'thumbnail' : 'http://mmaartwork.wackwack.co.uk/fightcasts/MMAInsideTheCageTV.jpg',
+        'fanart'    : '', } ,
+
+    {   'title'     : 'MMA Nuts (RSS)',
+        'url'       : 'rss://mmanuts.com:80/feed/podcast',
+        'thumbnail' : 'http://mmaartwork.wackwack.co.uk/fightcasts/mmanuts.jpg',
+        'fanart'    : '', } ,
+
+    {   'title'     : 'The Reem (Vimeo)',
+        'url'       : 'plugin://plugin.video.vimeo/?path=/root/contacts&folder=true&contact=thereem&store=contact_options&',
+        'thumbnail' : 'http://mmaartwork.wackwack.co.uk/fightcasts/thereem.jpg',
+        'fanart'    : '', } ,
+
+    {   'title'     : 'NickTheFace MMA Promos (Youtube)',
+        'url'       : 'plugin://plugin.video.youtube/?path=/root/contacts&folder=true&contact=NickTheFace2&store=contact_options&',
+        'thumbnail' : 'http://mmaartwork.wackwack.co.uk/fightcasts/NickTheFace2.jpg',
+        'fanart'    : '', } ,
+
+    {   'title'     : 'StillW1ll (Youtube)',
+        'url'       : 'plugin://plugin.video.youtube/?path=/root/contacts&folder=true&contact=StillW1ll&store=contact_options&',
+        'thumbnail' : 'http://mmaartwork.wackwack.co.uk/fightcasts/stillwill.jpg',
+        'fanart'    : '', } ,
+
+    {   'title'     : 'The Fight Nerd (Youtube)',
+        'url'       : 'plugin://plugin.video.youtube/?path=/root/contacts&folder=true&contact=TheFightNerd&store=contact_options&',
+        'thumbnail' : 'http://mmaartwork.wackwack.co.uk/fightcasts/TheFightNerd.jpg',
+        'fanart'    : '', } ,
+
+    {   'title'     : 'The Fight Network (Youtube)',
+        'url'       : 'plugin://plugin.video.youtube/?path=/root/contacts&folder=true&contact=TheFightNetwork&store=contact_options&',
+        'thumbnail' : 'http://mmaartwork.wackwack.co.uk/fightcasts/TheFightNetwork.jpg',
+        'fanart'    : '', } ,
+
+    {   'title'     : 'UFC (Youtube)',
+        'url'       : 'plugin://plugin.video.youtube/?path=/root/contacts&folder=true&contact=UFC&store=contact_options&',
+        'thumbnail' : 'http://mmaartwork.wackwack.co.uk/fightcasts/ufc.jpg',
+        'fanart'    : 'http://mmaartwork.wackwack.co.uk/promotions/UltimateFightingChampionship-fanart.jpg', } ,
+
+    {   'title'     : 'WHOA! TV (Youtube)',
+        'url'       : 'plugin://plugin.video.youtube/?path=/root/contacts&folder=true&contact=whoatv&store=contact_options&',
+        'thumbnail' : 'http://mmaartwork.wackwack.co.uk/fightcasts/whoatv.jpg',
+        'fanart'    : '', } ,
+
+    {   'title'     : 'www.MMA30.com (Youtube)',
+        'url'       : 'plugin://plugin.video.youtube/?path=/root/contacts&folder=true&contact=MMA30tv&store=contact_options&',
+        'thumbnail' : 'http://mmaartwork.wackwack.co.uk/fightcasts/MMA30tv.jpg',
+        'fanart'    : '', } ,
+
+]
