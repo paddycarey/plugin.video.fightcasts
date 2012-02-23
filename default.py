9a4359199a8e74ea98fa7e78597eb3b3f20c2742
Patrick Carey <paddy@wackwack.co.uk>
2012-02-23 10:32:02 +0000

    Sort feed list by title before displaying

--- default.py
+++ default.py
@@ -7,12 +7,12 @@ import xbmcaddon
 import xbmcgui
 import xbmcplugin
 
+from operator import itemgetter
 from resources.lib import feeds
 
 ### get addon info
 __addon__             = xbmcaddon.Addon()
-__addonidint__        = int(sys.argv[1])
-__addonid__         = __addon__.getAddonInfo('id')
+__addonint__        = int(sys.argv[1])
 __addondir__          = xbmc.translatePath(__addon__.getAddonInfo('path'))
 
 
@@ -28,24 +28,26 @@ def addFeed(title, url, thumbnail = '', fanart = ''):
 
     li = xbmcgui.ListItem( label = title, iconImage = thumbnail, thumbnailImage = thumbnail)
     li.setProperty( "Fanart_Image", fanart )
-    xbmcplugin.addDirectoryItem( handle = __addonidint__, url = url, listitem = li, isFolder = True)
+    xbmcplugin.addDirectoryItem( handle = __addonint__, url = url, listitem = li, isFolder = True)
 
 
 if __name__ == "__main__":
     
+    # get feed type
     feedType = sys.argv[2].lstrip('?')
     
     if feedType == 'video':
         # add feeds to directory listing
-        for feed in feeds.video:
+        for feed in sorted(feeds.video, key=itemgetter('title')):
             addFeed( title = feed['title'], url = feed['url'], thumbnail = feed['thumbnail'], fanart = feed['fanart'] )
     elif feedType == 'audio':
         # add feeds to directory listing
-        for feed in feeds.audio:
+        for feed in sorted(feeds.audio, key=itemgetter('title')):
             addFeed( title = feed['title'], url = feed['url'], thumbnail = feed['thumbnail'], fanart = feed['fanart'] )
     else:
+        # add main menu items
         addFeed( title = 'Audio', url = 'plugin://plugin.video.fightcasts/?audio', thumbnail = 'DefaultAudio.png')
         addFeed( title = 'Video', url = 'plugin://plugin.video.fightcasts/?video', thumbnail = 'DefaultVideo.png')
 
     ## finish adding items to list and display
-    xbmcplugin.endOfDirectory(__addonidint__)
+    xbmcplugin.endOfDirectory(__addonint__)
